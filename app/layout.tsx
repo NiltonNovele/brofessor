55fa7fc3503d922c7d3fb8eb4d38c93eb8aec695
Nilton Novele <nilton.novele@gmail.com>
2025-06-25 05:14:57 +0200

    server side rendering refres

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,6 @@ import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-
-
 const bricolage = Bricolage_Grotesque({
   variable: "--font-bricolage",
   subsets: ["latin"],
@@ -16,16 +14,22 @@ export const metadata: Metadata = {
   description: "Your AI study Bro",
 };
 
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  console.log('Clerk publishable key:', process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY);
+
+
   return (
     <html lang="en">
       <body className={`${bricolage.variable} antialiased`}>
-        <ClerkProvider appearance={{ variables: { colorPrimary: "#fe5933" } }}>
+        <ClerkProvider
+          publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+          appearance={{ variables: { colorPrimary: "#fe5933" } }}
+        >
           <Navbar />
           {children}
           <Footer />
